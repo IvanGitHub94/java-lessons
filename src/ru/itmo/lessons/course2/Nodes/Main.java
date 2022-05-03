18152617c463f60add26dbcb71ee7b06fb2467c7
Ivan Smirnov <smirnov_i94@mail.ru>
2022-05-03 22:15:04 +0300

    COURSE 2 (заготовка, тест)

---
+++ src/ru/itmo/lessons/course2/Nodes/Main.java
@@ -0,0 +1,18 @@
+package ru.itmo.lessons.course2.Nodes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(3, "John");
+        tree.insert(8, "T1000");
+        tree.insert(1, "Sara");
+        tree.insert(2, "T800");
+
+        Node findJohn = tree.find(3);
+        findJohn.printNode();
+        System.out.println("====");
+        tree.print(findJohn);
+    }
+
+}
