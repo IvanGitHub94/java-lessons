3c50f6bcc05e6e0faf470dd8aa41edee8daefcda
Ivan Smirnov <smirnov_i94@mail.ru>
2022-05-11 00:42:16 +0300

    COURSE 2 UPD (готово, скорректировано)

--- src/ru/itmo/lessons/course2/Nodes/Node.java
+++ src/ru/itmo/lessons/course2/Nodes/Node.java
@@ -1,7 +1,7 @@
 package ru.itmo.lessons.course2.Nodes;
 
 public class Node {
-    public int key; //ключ узла
+    public int key; //ключ узла, без ключа бинарное древо поиска не может существовать
     public String data; //некоторые данные в узле
     public Node leftChild; //левый потомок
     public Node rightChild; //правый потомок
